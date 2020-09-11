e14e21495602ee6fdc0d68ca71a010ab1a616d01
Aditya <adipimpalkar14@gmail.com>
2020-09-11 08:51:44 +0530

    Updates

---
+++ precache-manifest.e38f7893b39d72f82a62a07e425fd6be.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c8df66bef7182604cbda62c9d3e12e4",
+    "url": "/foodiefastfoods/index.html"
+  },
+  {
+    "revision": "c1f91117a2f6e2287cee",
+    "url": "/foodiefastfoods/static/css/2.b128bc40.chunk.css"
+  },
+  {
+    "revision": "c4b8489797dc7e5bf176",
+    "url": "/foodiefastfoods/static/css/main.93562dc3.chunk.css"
+  },
+  {
+    "revision": "c1f91117a2f6e2287cee",
+    "url": "/foodiefastfoods/static/js/2.a5950704.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/foodiefastfoods/static/js/2.a5950704.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c4b8489797dc7e5bf176",
+    "url": "/foodiefastfoods/static/js/main.161346ba.chunk.js"
+  },
+  {
+    "revision": "48507c388ccfd07497d6",
+    "url": "/foodiefastfoods/static/js/runtime-main.8f930960.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/foodiefastfoods/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/foodiefastfoods/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/foodiefastfoods/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/foodiefastfoods/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/foodiefastfoods/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
